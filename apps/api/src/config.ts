b4fde3ee6ba4325fa951d33f6a688027827ace55
i-nafrizaldi <ivannafrizaldi@gmail.com>
2024-06-21 23:42:38 +0700

    feat: fix config

--- apps/api/src/config.ts
+++ apps/api/src/config.ts
@@ -11,8 +11,9 @@ config({ path: resolve(__dirname, `../${envFile}.local`), override: true });
 // Load all environment variables from .env file
 
 export const PORT = process.env.PORT || 8000;
-console.log('PORT API', PORT);
 export const DATABASE_URL = process.env.DATABASE_URL || '';
 export const NEXT_BASE_URL = process.env.NEXT_BASE_URL;
 export const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
 export const GMAIL_APP_PASSWORD = process.env.GMAIL_APP_PASSWORD;
+export const GOOGLE_CLIENT_ID = process.env.CLIENT_ID;
+export const GOOGLE_CLIENT_SECRET = process.env.CLIENT_SECRET;
--- apps/web/src/utils/config.ts
+++ apps/web/src/utils/config.ts
@@ -10,10 +10,10 @@ config({ path: resolve(__dirname, `../${envFile}.local`), override: true });
 
 // Load all environment variables from .env file
 
-export const NEXT_PUBLIC_BASE_API_URL =
+export const BASE_API_URL =
   process.env.NEXT_PUBLIC_BASE_API_URL || 'http://localhost:8000/api';
 export const NEXT_PUBLIC_BASE_URL =
-  process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000';
+  process.env.BASE_URL || 'http://localhost:3000';
 export const PORT = process.env.PORT || 3000;
 export const DATABASE_URL = process.env.DATABASE_URL || '';
 export const GMAIL_EMAIL = process.env.GMAIL_EMAIL;
